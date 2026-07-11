f74a04c6befa09145ee19dbf7c7417b7ac1b26b5
fahriaprlianalfarizy <alfarizyfahri210407@gmail.com>
2026-07-12 04:26:25 +0700

    Menghilangkan fungsi CSRF Fahri

--- config/function.php
+++ config/function.php
@@ -33,8 +33,6 @@ function aman($data)
     return htmlspecialchars($data ?? '', ENT_QUOTES, 'UTF-8');
 }
 
-
-
 function ambil_data_procedure($koneksi, $sql)
 {
     $data = [];
@@ -53,32 +51,8 @@ function ambil_data_procedure($koneksi, $sql)
     return $data;
 }
 
-function buat_csrf_token(): string
+function format_jam($nilai)
 {
-    if (
-        !isset($_SESSION['csrf_token']) ||
-        !is_string($_SESSION['csrf_token'])
-    ) {
-        $_SESSION['csrf_token'] =
-            bin2hex(random_bytes(32));
-    }
-
-    return $_SESSION['csrf_token'];
-}
-
-function validasi_csrf_token(?string $token): bool
-{
-    if (
-        !isset($_SESSION['csrf_token']) ||
-        !is_string($_SESSION['csrf_token']) ||
-        !is_string($token)
-    ) {
-        return false;
-    }
-
-    return hash_equals(
-        $_SESSION['csrf_token'],
-        $token
-    );
+    return number_format((float) $nilai, 1, ',', '.');
 }
 ?>
